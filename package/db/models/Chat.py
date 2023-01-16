81c31b0c1574850436701f5befd50b1c4f720aa5
batreller <batreller@gmail.com>
2023-01-16 20:41:57 +0000

    fix Chat data structure

--- package/db/models/Chat.py
+++ package/db/models/Chat.py
@@ -1,9 +1,15 @@
 from tortoise import fields
 from tortoise.models import Model
 
+from package.db.models.User import User
 from package.db.models.mixins.Timestamp import TimestampMixin
 
 
 class Chat(Model, TimestampMixin):
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
+    first_user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
+        'models.User'
+    )
+    second_user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
+        'models.User'
+    )
