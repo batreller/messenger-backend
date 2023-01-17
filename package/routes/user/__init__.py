9fa4c8cf12734b205e7789e2942d89f07a0b27ac
batreller <batreller@gmail.com>
2023-01-17 17:43:27 +0000

    implement PATCH /user/bio endpoint

--- package/routes/user/__init__.py
+++ package/routes/user/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from package.routes.user.endpoints import login
 from package.routes.user.endpoints import me
 from package.routes.user.endpoints import register
+from package.routes.user.endpoints import bio
 from package.routes.user.exceptions import user_exists
 from package.routes.user.exceptions import user_exists, wrong_credentials
 from package.routes.user.hasher import ph
@@ -15,3 +16,4 @@ router = APIRouter(prefix='/user')
 router.include_router(register.router)
 router.include_router(login.router)
 router.include_router(me.router)
+router.include_router(bio.router)
--- package/routes/user/exceptions.py
+++ package/routes/user/exceptions.py
@@ -2,6 +2,7 @@ from fastapi import status
 from pydantic import Field
 
 from package.exceptions.InvalidData.InvalidDataException import InvalidDataException
+from package.routes.user.inputs.BioInput import BioInput
 from package.routes.user.inputs.LoginInput import LoginInput
 from package.routes.user.inputs.RegisterInput import RegisterInput
 
@@ -10,20 +11,28 @@ class PartialRegister(RegisterInput):
     password: str | None = Field(required=False, default=None)
 
 
-user_exists_message = "This user already exists."
+USER_EXISTS_MESSAGE = "This user already exists."
+WRONG_CREDENTIALS_MESSAGE = "Wrong login credential, try again."
+INVALID_BIO_MESSAGE = "Bio is too long"
+
 user_exists = InvalidDataException[PartialRegister](
     status_code=status.HTTP_400_BAD_REQUEST,
     code="USER_EXISTS",
-    message=user_exists_message,
+    message=USER_EXISTS_MESSAGE,
     field_error_mapping=PartialRegister(
-        username=user_exists_message,
-        email=user_exists_message,
+        username=USER_EXISTS_MESSAGE,
+        email=USER_EXISTS_MESSAGE,
     )
 ).populate_errors_with_message()
-
 
 wrong_credentials = InvalidDataException[LoginInput](
     status_code=status.HTTP_400_BAD_REQUEST,
     code="WRONG_CREDENTIALS",
-    message="Wrong login credential, try again."
+    message=WRONG_CREDENTIALS_MESSAGE
+).populate_errors_with_message()
+
+invalid_bio = InvalidDataException[BioInput](
+    status_code=status.HTTP_400_BAD_REQUEST,
+    code="INVALID_BIO",
+    message=INVALID_BIO_MESSAGE
 ).populate_errors_with_message()
